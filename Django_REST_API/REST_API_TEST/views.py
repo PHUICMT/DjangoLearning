2fecfd1260524b6e16e8a127932ec0e302ad899f
PHUICMT <icmtchannel@gmail.com>
2021-05-08 11:46:53 +0700

    REST_Framework setup Completed

--- Django_REST_API/REST_API_TEST/views.py
+++ Django_REST_API/REST_API_TEST/views.py
@@ -1,16 +1,16 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from REST_API_TEST.serializes import UserSerializer, GroupSerializer
+from REST_API_TEST.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [Permission.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [Permission.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
